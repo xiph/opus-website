37f14b697c61a29cffb4fa008d3890dacae62621
Gregory Maxwell <greg@xiph.org>
2013-12-04 23:38:26 +0000

    Add 1.1 docs
    git-svn-id: http://svn.xiph.org/websites/opus-codec.org@19037 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ docs/html_api-1.1.0/search/defines_5f.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['_5f_5fopus_5fcheck_5fdecstate_5fptr',['__opus_check_decstate_ptr',['../opus__multistream_8h.html#afa5a8e09ef3853e1d0a249868e92e730',1,'opus_multistream.h']]],
+  ['_5f_5fopus_5fcheck_5fencstate_5fptr',['__opus_check_encstate_ptr',['../opus__multistream_8h.html#afabd641a2bc6c09c9517294be87351c2',1,'opus_multistream.h']]],
+  ['_5f_5fopus_5fcheck_5fint',['__opus_check_int',['../opus__defines_8h.html#a5e06ccb0a22aa1e8812074924ce0c69a',1,'opus_defines.h']]],
+  ['_5f_5fopus_5fcheck_5fint_5fptr',['__opus_check_int_ptr',['../opus__defines_8h.html#a4af8c8370e107f50db6de22dc6f1fafb',1,'opus_defines.h']]],
+  ['_5f_5fopus_5fcheck_5fuint_5fptr',['__opus_check_uint_ptr',['../opus__defines_8h.html#a07dfb6cf9c1c44e0df7d3d9056699969',1,'opus_defines.h']]],
+  ['_5f_5fopus_5fcheck_5fval16_5fptr',['__opus_check_val16_ptr',['../opus__defines_8h.html#a179b58ed56d72e609c64bb064b68d7df',1,'opus_defines.h']]]
+];
